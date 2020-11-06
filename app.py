2734ee64b2f3d9ffe0a2a66c0c071b43beb481d6
Jason Eccles <ec2-user@ip-172-31-23-90.ec2.internal>
2020-11-06 02:59:06 +0000

    added twilio incoming message and response logic using http post request to /bot endpoint

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import os
 from os.path import join, dirname
+from twilio.rest import Client
 import flask
+from flask import request
+import requests
 import flask_socketio
 import flask_sqlalchemy
 from dotenv import load_dotenv
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = flask.Flask(__name__)
 socketio = flask_socketio.SocketIO(app)
@@ -26,9 +30,35 @@ import models
 @app.route('/', methods=['GET', 'POST'])
 def hello():
     return flask.render_template('index.html')
+    
+@app.route('/bot', methods=['POST'])
+def bot():
+    #twilio
+    incoming_msg = request.values.get('Body', '').lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+    if 'quote' in incoming_msg:
+        # return a quote
+        r = requests.get('https://api.quotable.io/random')
+        if r.status_code == 200:
+            data = r.json()
+            quote = f'{data["content"]} ({data["author"]})'
+        else:
+            quote = 'I could not retrieve a quote at this time, sorry.'
+        msg.body(quote)
+        responded = True
+    if 'cat' in incoming_msg:
+        # return a cat pic
+        msg.media('https://cataas.com/cat')
+        responded = True
+    if not responded:
+        msg.body('I only know about famous quotes and cats, sorry!')
+    return str(resp)
+
 
 if __name__ == '__main__':
-    init_db(app)
+    #init_db(app)
     socketio.run(
         app,
         host=os.getenv('IP', '0.0.0.0'),
