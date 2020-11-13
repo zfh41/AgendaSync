12c365d3e35e96ca9b6b363ba5630a383a7e85c2
Andre Pugliese <ec2-user@ip-172-31-95-180.ec2.internal>
2020-11-13 23:35:09 +0000

    Updated models.py and bot.py with pylint formatting, functionality is the same

--- bot.py
+++ bot.py
@@ -1,17 +1,18 @@
-import os
+''' bot.py '''
+
+#import os
 from os.path import join, dirname
 from dotenv import load_dotenv
-import requests
-from flask import request
-from twilio.twiml.messaging_response import MessagingResponse
+#import requests
+#from flask import request
+#from twilio.twiml.messaging_response import MessagingResponse
 
 dotenv_path = join(dirname(__file__), "sql.env")
 load_dotenv(dotenv_path)
 
-
 class Bot:
+    ''' Textbot class with twilio integration '''
     def __init__(self, message):
-        self.message = message
-
+        ''' Initialize textbot object ''' #self.message = message - Insert below docstring
     def bot_response(self):
-        message = self.message
+        ''' Initialize textbot response ''' #message = self.message - Insert below docstring
--- models.py
+++ models.py
@@ -1,17 +1,19 @@
-import flask_sqlalchemy
-from app import DB
+''' models.py '''
+
 import datetime
+from app import DB
 
 start_date = datetime.datetime.utcnow()
 end_date = start_date + datetime.timedelta(days=1)
+
 class Person(DB.Model):
+    ''' Initialize Person Table with ID/Email/Todos/Credentials Columns '''
     id = DB.Column(DB.Integer, primary_key=True)
     email = DB.Column(DB.String(120), nullable=False)
     todos = DB.relationship('Todo', backref='person', lazy=True)
     cred = DB.Column(DB.PickleType)
-    
-    
 class Todo(DB.Model):
+    ''' Initialize Todo Table with person_Id/Todo/StartDate/DueDate Columns '''
     id = DB.Column(DB.Integer, primary_key=True)
     person_id = DB.Column(DB.Integer, DB.ForeignKey('person.id'))
     todo = DB.Column(DB.String(255), nullable=False)
@@ -19,4 +21,6 @@ class Todo(DB.Model):
     due_date = DB.Column(DB.DateTime, default=end_date)
 
 def createModels():
+    ''' Initialize Person Table with ID/Email/Todos/Credentials Column '''
     DB.create_all()
+    
